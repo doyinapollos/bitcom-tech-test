10d865d7687c090b3aeb930db04b9301d7a090b0
ebiapollos <doyinapollos@gmail.com>
2022-09-24 17:17:25 +0100

    summary section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,34 @@ function App() {
   return (
     <div className="app">
         
+        <div className='header'>
+          <h2> <u>APOLLOS EBI-EREFA </u></h2>
+          <h3>Front end Developer</h3>
+
+        </div>
+
+        <div className='main--section'>
+
+          <aside className='aside--left'>
+
+            <section className='summary--section'>
+              <p>
+              I am an enthusiastic and detail-oriented Frontend Software Engineer<br /> 
+              seeking an entry-level position with Company to use my skills in <br /> 
+              coding, troubleshooting complex problems, and assisting in the <br /> 
+              timely completion of projects.
+              </p>
+
+            </section>
+
+          </aside>
+
+          <aside className='aside--right'>
+
+          </aside>
+
+
+        </div>
 
 
     </div>
